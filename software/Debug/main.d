cdfabd1a259e98bd65b6d878e696509cde0794fc
Li Wing <liwramos1@gmail.com>
2022-04-03 11:30:27 -0300

    added sw folder

---
+++ software/Debug/main.d
@@ -0,0 +1,168 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/driverlib.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/inc/hw_memmap.h
+main.obj: D:/ti/ccs1110/ccs/ccs_base/msp430/include/msp430.h
+main.obj: D:/ti/ccs1110/ccs/ccs_base/msp430/include/msp430fr6972.h
+main.obj: D:/ti/ccs1110/ccs/ccs_base/msp430/include/in430.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+main.obj: D:/ti/ccs1110/ccs/ccs_base/msp430/include/msp430fr5xx_6xxgeneric.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdint.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_stdint40.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/stdint.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_stdint.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_stdint.h
+main.obj: D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdbool.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/esi.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/eusci_b_i2c.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/cs.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/aes256.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/uups.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/framctl_a.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/timer_b.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/framctl.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/eusci_a_spi.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/eusci_a_uart.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/tlv.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/timer_a.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/pmm.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/adc12_b.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/sysctl.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/sfr.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/ref_a.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/mpy32.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/wdt_a.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/saph.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/crc32.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/mpu.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/mtif.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/lcd_c.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/rtc_c.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/gpio.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/rtc_b.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/comp_e.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/ram.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/saph_a.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/dma.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/hspll.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/eusci_b_spi.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/sdhs.h
+main.obj: D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/crc.h
+
+../main.c:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/driverlib.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/inc/hw_memmap.h:
+
+D:/ti/ccs1110/ccs/ccs_base/msp430/include/msp430.h:
+
+D:/ti/ccs1110/ccs/ccs_base/msp430/include/msp430fr6972.h:
+
+D:/ti/ccs1110/ccs/ccs_base/msp430/include/in430.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
+D:/ti/ccs1110/ccs/ccs_base/msp430/include/msp430fr5xx_6xxgeneric.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdint.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_stdint40.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/stdint.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_stdint.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_stdint.h:
+
+D:/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdbool.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/esi.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/eusci_b_i2c.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/cs.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/aes256.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/uups.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/framctl_a.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/timer_b.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/framctl.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/eusci_a_spi.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/eusci_a_uart.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/tlv.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/timer_a.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/pmm.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/adc12_b.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/sysctl.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/sfr.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/ref_a.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/mpy32.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/wdt_a.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/saph.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/crc32.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/mpu.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/mtif.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/lcd_c.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/rtc_c.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/gpio.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/rtc_b.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/comp_e.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/ram.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/saph_a.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/dma.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/hspll.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/eusci_b_spi.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/sdhs.h:
+
+D:/Users/liwka/workspace_v11/Magnetorquer/driverlib/MSP430FR5xx_6xx/crc.h:
+
